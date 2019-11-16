6d04a0227168112c2392e8417f89779f5aa38268
Rama_Karri <Rama_Karri@EPINHYDW0322.princeton.epam.com>
2019-11-16 20:36:59 +0530

    simplified the logic

--- src/main/java/com/epam/course/controller/MyProductsController.java
+++ src/main/java/com/epam/course/controller/MyProductsController.java
@@ -9,67 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MyProductsController {
 	
-	/*@Autowired
-	private transient MyProductService myProdService;*/
 	
 	@GetMapping("/status")
 	public ResponseEntity<String> getDeployStatus()	{					
 			return ResponseEntity.ok("<!DOCTYPE html>\n" + 
 					"<html>\n" + 
 					"<head>\n" + 
-					"<title>Cloud N DevOps Hackathon Event 2019</title>\n" + 
+					"<title>My Page</title>\n" + 
 					"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" + 
 					"<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">\n" + 
 					"</head>\n" + 
 					"<body>\n" + 
 					"\n" + 
-					"<h2>Cloud N DevOps Hackathon Event 2019</h2>\n" + 
+					"<h2>My page</h2>\n" + 
 					"<div class=\"w3-container w3-green\">\n" + 
-					"<p font>Team A :  Green deployment application</p>\n" + 
+					"<p font> My Blue application</p>\n" + 
 					"</div>\n" + 
 					"</body>\n" + 
 					"</html>");	
 	}
 	
-	/*@GetMapping("/myProducts")
-	public ResponseEntity<List<MyProduct>> getMyProducts()	{
-		
-		if(!myProdService.getMyProducts().isEmpty()) {
-			return ResponseEntity.ok(myProdService.getMyProducts());
-		} else {			
-			return ResponseEntity.ok(ProductHelper.buildErrMsg());
-		}
-		
-	}
-	
-	@PostMapping("/myProducts")
-	public ResponseEntity<MyProduct> saveMyProducts(@RequestBody MyProduct product)
-	{	
-		return ResponseEntity.ok(myProdService.saveMyProduct(product));
-		
-	}
-	
-	@GetMapping("/myProducts/{prodId}")
-	public ResponseEntity<Optional<MyProduct>> getMyProduct(final @PathVariable long prodId)
-	{
-		Optional<MyProduct> product = myProdService.getMyProduct(prodId);
-		if(!product.isPresent()) {	
-			return ResponseEntity.ok(ProductHelper.buildErrMsg(prodId, ProductCodes.PROD_NOT_AVAIL));
-		} else {
-			return ResponseEntity.ok(myProdService.getMyProduct(prodId));
-		}
-		
-	}
-	
-	@DeleteMapping("/myProducts/{prodId}")
-	public ResponseEntity<?> removeMyProduct(final @PathVariable long prodId)
-	{
-		Optional<MyProduct> product = myProdService.removeMyProduct(prodId);
-		
-		if(!product.isPresent())			
-			return ResponseEntity.ok(ProductHelper.buildErrMsg(prodId, ProductCodes.PROD_NOT_DEL));
-		else
-			return ResponseEntity.ok(ProductHelper.buildErrMsg(prodId, ProductCodes.PROD_DEL_SUCCESS));	
-	}*/
-
 }
--- src/test/java/com/epam/course/MyProductControllerTests.java
+++ src/test/java/com/epam/course/MyProductControllerTests.java
@@ -1,126 +1,13 @@
 package com.epam.course;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import com.epam.course.controller.MyProductsController;
-import com.epam.course.model.MyProduct;
-import com.epam.course.service.MyProductService;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest(MyProductsController.class)
+@SpringBootTest
 public class MyProductControllerTests {
 	
-	@Autowired
-	private MockMvc mockMvc;
-
-	@MockBean
-	private transient MyProductService myProdService;
-	
-	static MyProduct mockProd1 = new MyProduct(1, "prod name", "prod desc");
-	static MyProduct mockProd2 = new MyProduct(2, "prod name2", "prod desc2");
-	static MyProduct mockProd3 = new MyProduct(3, "", "prod desc3");
-	static List<MyProduct> originalProdList = new ArrayList<>();
-	static JSONObject mockProdJson;
-	
-	@BeforeClass
-	public static void initialize() throws JSONException {	
-			
-		originalProdList.add(mockProd1);		
-		mockProdJson = new JSONObject();
-		mockProdJson.accumulate("prodId", 1);
-		mockProdJson.accumulate("name", "prod name");
-		mockProdJson.accumulate("desc", "prod desc");	
-		
-	}
-	
 	@Test
-	public void getMyProductsTest() throws Exception {
-		
-		Mockito.when(myProdService.getMyProducts()).thenReturn((originalProdList));
-
-		JSONArray mockResponse = new JSONArray();
-		mockResponse.put(mockProdJson);
-
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/myProducts")
-		        .accept(MediaType.APPLICATION_JSON);
-		
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();	
-
-		JSONAssert.assertEquals(mockResponse.toString(), result.getResponse().getContentAsString(), true);
-	}
-	
-	@Test
-	public void getMyProductTest() throws Exception {		
-		
-		Mockito.when(myProdService.getMyProduct(1L)).thenReturn(Optional.of(mockProd1));
-
-		JSONArray mockResponse = new JSONArray();
-		mockResponse.put(mockProdJson);		
-
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/myProducts/1")
-		        .accept(MediaType.APPLICATION_JSON);
-		
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();	
-
-		JSONAssert.assertEquals(mockResponse.get(0).toString(), result.getResponse().getContentAsString(), true);
-	}
-	
-	@Test
-	public void saveMyProductsTest() throws Exception {		
-		
-		Mockito.when(myProdService.saveMyProduct(Mockito.any())).thenReturn(mockProd2);
-
-		JSONObject mockSuceessResponse = new JSONObject();
-		mockSuceessResponse.accumulate("prodId", 2);
-		mockSuceessResponse.accumulate("name", "prod name2");
-		mockSuceessResponse.accumulate("desc", "prod desc2");
-		
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/myProducts").content(mockSuceessResponse.toString())
-		        .contentType(MediaType.APPLICATION_JSON)
-		        .accept(MediaType.APPLICATION_JSON);
-		
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();			
-
-		JSONAssert.assertEquals(mockSuceessResponse.toString(), result.getResponse().getContentAsString(), true);
-	}
-	
-	@Test
-	public void removeMyProductTest() throws Exception {			
-		
-		Mockito.when(myProdService.removeMyProduct(3L)).thenReturn(Optional.of(mockProd3));
-
-		JSONObject mockSuceessResponse = new JSONObject();
-		mockSuceessResponse.accumulate("prodId", 3);
-		mockSuceessResponse.accumulate("name", null);
-		mockSuceessResponse.accumulate("desc", "Product deleted successfully");
-		
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/api/myProducts/3").content(mockSuceessResponse.toString())			
-		      	.accept(MediaType.APPLICATION_JSON);
-		
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();		
-		System.out.println("original Response --> "+ result.getResponse().getContentAsString());
-
-		JSONAssert.assertEquals(mockSuceessResponse.toString(), result.getResponse().getContentAsString(), false);
+	void contextLoads() {
 	}
 
 }
